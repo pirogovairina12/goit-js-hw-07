02790dadb9105a10a52171fa21d9f7a4a5f54cfc
Anastasia Pirogova <120117437+pirogovairina12@users.noreply.github.com>
2023-04-20 14:15:42 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -37,29 +37,29 @@ const img = document.querySelector(".gallery__image");
 
 gallery.addEventListener('click', onGalleryClick);
 
+
 function onGalleryClick(e) {
     e.preventDefault();
 
     if (e.target.nodeName !== 'IMG') {
         return;
     }
+    const largeImageURL = e.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${largeImageURL}" width="800" height="600">`);
 
-    const instance = basicLightbox.create(`
-    <img src="${e.target.original}">
-  `);
 
     instance.show();
-    document.addEventListener("keydown", (event) => onEscPress(event, instance));
+    document.addEventListener('keydown', (event) => onEscPress(event, instance));
 }
 
-const onEscPress = (event, instance) => {
-    const ESC_KEYCODE = "Escape";
+function onEscPress(event, instance) {
+    const ESC_KEYCODE = 'Escape';
+
     if (event.code === ESC_KEYCODE) {
         instance.close();
-        instance.splice(instance.indexOf(instance), 1);
-        document.removeEventListener("keydown", (event) =>
-            onEscPress(event, instance)
-        );
+        document.removeEventListener('keydown', (event) => onEscPress(event, instance));
     }
-};
+}
+
+gallery.addEventListener('click', onGalleryClick);
 
